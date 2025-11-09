5dc117dbaeb7ffa1a8ebc39c20328e3c4dc85b43
Piotr Zając <piotrzaj@hotmail.com>
2025-11-09 21:49:49 +0100

    UpdaPSWindowsUpdate.ps1

--- RemotePSWindowsUpdate.ps1
+++ RemotePSWindowsUpdate.ps1
@@ -1,17 +1,12 @@
-$Username = 'numed.local\Administrator'
-$Password = 'Banan3#14'
-$Pass = ConvertTo-SecureString -AsPlainText $Password -Force
-$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Pass
-Enter-PSSession -ComputerName localhost -Credential $Cred
-
 Install-Module -Name PSWindowsUpdate
 Set-ExecutionPolicy RemoteSigned
 Import-Module PSWindowsUpdate
+Get-Command –module PSWindowsUpdate
 Get-WindowsUpdate
 Install-WindowsUpdate
 
 
 
-Get-Command –module PSWindowsUpdate
+
 
 

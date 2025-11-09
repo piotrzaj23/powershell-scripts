eca107e41093f6891aa75f48f2511a7689ca19cd
Piotr Zając <piotrzaj@hotmail.com>
2025-11-09 21:47:52 +0100

    Update PS_RemoteSession.ps1

--- PS RemoteSession.ps1
+++ PS RemoteSession.ps1
@@ -4,13 +4,3 @@ $Pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$Pass
 Enter-PSSession -ComputerName localhost -Credential $Cred
 
-Install-Module -Name PSWindowsUpdate
-Set-ExecutionPolicy RemoteSigned
-Import-Module PSWindowsUpdate
-Get-WindowsUpdate
-Install-WindowsUpdate
-
-
-
-Get-Command –module PSWindowsUpdate
-

a6ba860595b01a789ede4eea0aeb7250cf2becc0
Piotr Zając <piotrzaj@hotmail.com>
2025-11-09 15:56:39 +0100

    Add files via upload

---
+++ ChangeExtnsionOfFiles.ps1
@@ -0,0 +1,2 @@
+$dzisiejszaData = Get-Date -Format "yyyy-MM-dd"
+Get-ChildItem -Path "C:\Users\piotr\Downloads\*.txt" | Where-Object {$_.CreationTime.Date -eq $dzisiejszaData} | Rename-Item -NewName {($_.name -replace '\.txt$', '.klx')}
